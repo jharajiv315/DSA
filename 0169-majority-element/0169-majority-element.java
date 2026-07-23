dfb75a5617c413617acfa5333d5e028b90c77ee6
Rajiv jha <jharajiv315@gmail.com>
2026-07-23 19:40:48 +0530

    Moore algorithm

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,26 @@
 class Solution {
        public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
 
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(nums[i]) && map.get(nums[i]) > nums.length/2) {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int n = nums.length;
+        int cnt =0;
+        int ele =nums[0];
+        for(int i = 0;i<n;i++){
+            if( cnt == 0) ele = nums[i];
+            if(nums[i]==ele) cnt++;
+            else cnt--;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i]) && map.get(nums[i]) > nums.length/2) {
-                return nums[i];
-            }
-        }
-        return -1;
+        return ele;
     }
     
 }
