8a8d8a48f701c71b3ce497625d48e2055b4f6be2
Rajiv jha <jharajiv315@gmail.com>
2026-07-25 12:15:34 +0530

    Single nnumber

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single=0;
+        for(int i=0;i<nums.length;i++){
+            single^=nums[i];
+        }
+        return single;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
