dc761cec4b55eaa126b99cb07bea13cc52a7b378
Rajiv jha <jharajiv315@gmail.com>
2026-07-28 18:36:05 +0530

    Added solution

---
+++ 700272-count-zeros-in-a-sorted-matrix-GFG/700272-count-zeros-in-a-sorted-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countZeros(int[][] mat) {
+        // code here
+        int count = 0;
+        int n = mat.length;
+        for(int i = 0;i<n;i++){
+            for(int j = 0; j< n;j++){
+                if(mat[i][j] == 0){
+                    count ++;
+                }
+                
+            }
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
