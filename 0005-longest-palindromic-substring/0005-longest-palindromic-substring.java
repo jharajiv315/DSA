865b7c8b907134621c72c553589fc8b8dadd0eec
Rajiv jha <jharajiv315@gmail.com>
2026-09-18 18:25:41 +0530

    Time: 21 ms (35%), Space: 47 MB (22.36%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String longestPalindrome(String str) {
+       
+    if (str.length() <= 1)
+      return str;
+
+    String LPS = "";
+
+    for (int i = 1; i < str.length(); i++) {
+
+      // Consider odd length
+      int low = i;
+      int high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      String palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+
+      // Consider even length
+      low = i-1;
+      high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+    }
+
+    return LPS;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
