149590691a1fe9581a0f8fd330eb3d7d6225cff4
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:32 +0530

    Add memoized dynamic programming Fibonacci solution

---
+++ Dynamic_Programming/src/Fibonaaci.java
@@ -0,0 +1,18 @@
+
+public class Fibonaaci {
+    int[] dp ;
+    public int fibo(int n) {
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
+    }
+    public void fib(int n) {
+        dp = new int[n+1];
+        fibo(n);
+    }
+
+
+
+}
