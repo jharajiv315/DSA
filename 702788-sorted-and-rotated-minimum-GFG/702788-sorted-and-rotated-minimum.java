b81c0e1db28fcf56bb6769e3cbea0f1590c4cbc9
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 19:28:47 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 702788-sorted-and-rotated-minimum-GFG/702788-sorted-and-rotated-minimum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMin(int[] arr) {
+        // code here
+        int lo = 0;
+               int hi = arr.length -1;
+               int mn = Integer.MAX_VALUE;
+               while(lo<=hi){
+                   int mid = (lo+hi)/2;
+                  if(arr[lo] <= arr[mid]){
+                   mn  = Math.min(mn,arr[lo]);
+                   lo = mid + 1;
+                  }
+                  else{
+                       hi = mid - 1;
+                       mn = Math.min(mn,arr[mid]);
+                  }
+               }
+               return mn;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
