84f6e5681623566faa8b9fcceb53d9368bd396fc
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:39 +0530

    Add linked list stack reversal demonstration

---
+++ LinkedList/src/reversePush.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class reversePush {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        pushAtBottom(st);
+    }
+
+    private static void pushAtBottom(Stack<Integer> st) {
+
+    }
+}
