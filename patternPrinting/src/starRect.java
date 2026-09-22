c5675566f01c5fd59564338b7d73e6de71ffd33f
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:56 +0530

    Add solid star rectangle pattern printing

---
+++ patternPrinting/src/starRect.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class starRect {
+//   print this
+//    * * * *  *
+//    * * * *  *
+//    * * * *  *
+static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter rows");
+    int row = sc.nextInt();
+    System.out.println("Enter columns");
+    int col = sc.nextInt();
+    for (int i = 1; i <=row ; i++) {
+        for (int j = 1; j <=col ; j++) {
+
+        System.out.print("* ");
+        }
+
+        System.out.println();
+
+    }
+}
+}
