48bfa0be6a6f7ab30b5f6b20644960475106846f
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:53 +0530

    Add symmetric star pyramid pattern printing

---
+++ patternPrinting/src/Pyramid.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number ");
+        int n = sc.nextInt();
+        int nsp=n-1,nst=1;
+
+
+       //for (int i = 1; i <=n ; i++) {
+//            for (int j = 1; j <n+1-i ; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 1; j <=(2*i-1) ; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//
+//        }
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <nsp ; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=nst ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            nsp--;
+            nst+=2;
+
+        }
+    }
+}
