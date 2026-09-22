20fecd6eb4b947acb2801bf374d811adad129bfc
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:52:00 +0530

    Add queue basic operations demonstration

---
+++ queue/src/queueFunc.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queueFunc {
+    static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(5);
+    }
+
+
+}
