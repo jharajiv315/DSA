f005b3e81e47d6fe85ba788efaf74735f85c42f6
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:26 +0530

    Add 90-degree clockwise matrix rotation

---
+++ Arrays/src/rotateMatrix.java
@@ -0,0 +1,30 @@
+public class rotateMatrix {
+    static void main() {
+
+    }
+
+        // Function to rotate matrix clockwise by 90 degrees.
+        static void rotateby90(int[][] arr) {
+            int n = arr.length;
+            // transpose
+            for(int i=0;i<n;i++){
+                for(int j=0;j<i;j++){
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[j][i];
+                    arr[j][i] = temp;
+                }
+            }
+            // reverse each row
+            for(int row=0;row<n;row++){
+                int i=0, j=n-1;
+                while(i<j){
+                    int temp = arr[row][i];
+                    arr[row][i] = arr[row][j];
+                    arr[row][j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+        }
+    }
+
