23c5c0e329bdbe7ca3682823d32de21566f4988a
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:52:03 +0530

    Add missing element in range finder using ArrayList

---
+++ sets_and_maps/src/missingElementInRange.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class missingElementInRange {
+    static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(57);
+        arr.add(56);
+        int l=55;
+        int h=60;
+        int[] arr1 = {1,-2,6,7,5};
+
+      arr.add(  missingRange(arr,l,h));
+    }
+
+        static public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+            // code here
+
+
+    }
+}
