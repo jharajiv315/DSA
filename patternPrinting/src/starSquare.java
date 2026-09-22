dd7bcf2d5c4c299f287ee52071ad965d0552f6e6
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 20:51:56 +0530

    Add solid star square pattern printing

---
+++ patternPrinting/src/starSquare.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class starSquare {
+//    print this only one variable
+//    * * * * *
+//    * * * * *
+//    * * * * *
+static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number");
+    int n = sc.nextInt();
+
+    for (int i = 1; i <=n ; i++) {
+        for (int j = 1; j <=n ; j++) {
+
+            System.out.print("* ");
+        }
+
+        System.out.println();
+
+    }
+}
+}
