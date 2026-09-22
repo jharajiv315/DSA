686f44100ec4035a57948769a9b1509b319998e6
Rajiv jha <jharajiv315@gmail.com>
2026-09-22 17:51:49 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 878949-implement-upper-bound-GFG/878949-implement-upper-bound.java
@@ -0,0 +1,25 @@
+class Solution {
+    int upperBound(int[] arr, int tar) {
+        // code here
+        int lo = 0,hi= arr.length - 1;
+                      int idx = 0;
+
+                      while (lo<=hi){
+                          int mid = (lo+hi)/2;
+                          if(arr[mid]> tar) {
+                              hi = mid -1;
+
+                          }
+                          else if (arr[mid]<=tar) lo = mid+1;
+
+                      }
+
+                      return lo;
+
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
