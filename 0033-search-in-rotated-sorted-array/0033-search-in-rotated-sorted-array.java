ca6bad11d19a32f014e406b2b4780036cad8ca82
Rajiv jha <jharajiv315@gmail.com>
2026-09-23 20:13:56 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.49%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        // for(int i = 0 ; i< arr.length ; i++){
+        //     if(arr[i]==target) return i;
+        // }
+        // return -1;
+        int lo = 0;
+        int hi = arr.length -1;
+        
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(arr[mid] == target) return mid;
+           else if(arr[lo] <= arr[mid]){
+           if(arr[lo] <= target && arr[mid] >= target){
+            hi = mid -1;
+           }
+           else{
+
+            lo = mid + 1;
+           }
+           }
+           else{
+               if(arr[mid] <= target && arr[hi] >= target){
+                lo = mid + 1;
+           
+           }
+           else{
+
+             hi = mid -1;
+           }
+                
+           }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
