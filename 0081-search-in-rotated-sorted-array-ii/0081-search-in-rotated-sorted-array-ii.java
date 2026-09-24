1552bd1cb8b13751649f138fbca1adb529b203fa
Rajiv jha <jharajiv315@gmail.com>
2026-09-24 20:39:40 +0530

    Time: 0 ms (100%), Space: 45.2 MB (12.56%) - LeetHub - Time: O(n), Space: O(1)

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,7 +1,39 @@
 class Solution {
     public boolean search(int[] arr, int target) {
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i]==target) return true;
+        // for(int i = 0;i<arr.length;i++){
+        //     if(arr[i]==target) return true;
+        // }
+        // return false;
+         int lo = 0;
+        int hi = arr.length -1;
+        
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(arr[mid] == target) return true;
+           else if(arr[lo]==arr[mid]&& arr[mid]==arr[hi]) {
+                lo++;
+                hi--;
+            }
+           else if(arr[lo] <= arr[mid]){
+           if(arr[lo] <= target && arr[mid] >= target){
+            hi = mid -1;
+           }
+           else{
+
+            lo = mid + 1;
+           }
+           }
+           else{
+               if(arr[mid] <= target && arr[hi] >= target){
+                lo = mid + 1;
+           
+           }
+           else{
+
+             hi = mid -1;
+           }
+                
+           }
         }
         return false;
     }
