c5c31e3cf15364fffedb0655289bad7121d65fd5
Rajiv jha <jharajiv315@gmail.com>
2026-09-26 20:26:38 +0530

    Time: 0 ms (100%), Space: 119.4 MB (21.97%) - LeetHub - Time: O(n * log m), Space: O(1)

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -2,19 +2,39 @@ class Solution {
     public int[] findPeakGrid(int[][] arr) {
         int n = arr.length;
         int m = arr[0].length;
-        int lo = 1,hi = (m*n) - 2;
-        int max = Integer.MIN_VALUE;
-        int[] ans = new int[2];
-        for(int i = 0;i< n ; i++){
-            for(int j=0;j<m;j++){
-                if(max < arr[i][j]){
-                    max = arr[i][j];
-                    ans[0] = i;
-                    ans[1] = j;
-                }
+        int lo = 0, hi = m - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int row = max(arr, n, m, mid);
+
+            int left = mid - 1 >= 0 ? arr[row][mid - 1] : -1;
+            int right = mid + 1 < m ? arr[row][mid + 1] : -1;
+
+            if (arr[row][mid] > left && arr[row][mid] > right) {
+                return new int[]{row, mid};
+            } else if (arr[row][mid] < left) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
         }
-        return ans;
+
+        return new int[]{-1, -1};
+    }
+
+    public int max(int[][] arr, int n, int m, int mid) {
+        int max = -1;
+        int idx = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i][mid] > max) {
+                max = arr[i][mid];
+                idx = i;
+            }
+        }
+
+        return idx;
     }
 }
 
