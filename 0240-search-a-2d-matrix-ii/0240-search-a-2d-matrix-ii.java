7b2c90f17260dc749d938895b61eff30a4e14898
Rajiv jha <jharajiv315@gmail.com>
2026-09-26 19:33:02 +0530

    Time: 2 ms (100%), Space: 48.1 MB (64.33%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,26 +3,19 @@ class Solution {
         int n = mat.length;
         int m = mat[0].length;
         
-        
-       for(int i = 0; i < n; i++ ){
-        if(mat[i][0]==target) return true;
-        if(binarySearch(mat[i], target))
-        return true;
+        int row = 0;
+        int col = m-1;
+       while(row < n && col >= 0){
+        if(mat[row][col]==target) return true;
+        else if(mat[row][col] > target){
+            col--;
+            
+        }
+        else  row++;
+       
        }
         return false;
     }
-     public boolean binarySearch(int[] mat, int target){
-        int lo = 0;
-        int hi = mat.length - 1;
-        while(lo<=hi){
-            int mid = (lo + hi) / 2;
-            if(mat[mid] == target) return true;
-            else if(mat[mid] > target) hi = mid - 1;
-            else lo = mid + 1;
-
-        }
-        return false;
-     }
 }
 
 // Synced seamlessly with LeetHub Pro
